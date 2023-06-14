44d4a79c76dbc1bb3dd7aebddbbc643ffab5a9f9
Gaurav Kumar <gaurav.kumar1@cred.club>
2023-06-14 08:52:21 +0530

    add AHC engine

---
+++ asynchttpclient-benchmark/src/main/java/benchmark/asynchttpclient/Engine.java
@@ -0,0 +1,77 @@
+package benchmark.asynchttpclient;
+
+import com.ss.benchmark.httpclient.common.HttpClientEngine;
+import org.asynchttpclient.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CompletableFuture;
+
+import static com.ss.benchmark.httpclient.common.Exceptions.rethrowChecked;
+
+public class Engine implements HttpClientEngine {
+
+    private AsyncHttpClient client;
+    private String baseUrl;
+
+    @Override
+    public void createClient(String host, int port) {
+        AsyncHttpClientConfig config = new DefaultAsyncHttpClientConfig.Builder().setMaxConnections(MAX_CONNECTION_POOL_SIZE)
+                                                                                 .setConnectionTtl(-1)
+                                                                                 .setSoLinger(0)
+                                                                                 .setMaxConnectionsPerHost(MAX_CONNECTION_POOL_SIZE_PER_HOST)
+                                                                                 .setRequestTimeout(READ_TIMEOUT)
+                                                                                 .setConnectTimeout(CONNECT_TIMEOUT)
+                                                                                 .setReadTimeout(READ_TIMEOUT).build();
+        client  =  Dsl.asyncHttpClient(config);
+        baseUrl = url(host, port);
+    }
+
+    @Override
+    public String blockingGET(String path) {
+        return rethrowChecked(() -> nonblockingGET(path).get());
+    }
+
+    @Override
+    public String blockingPOST(String path, String body) {
+        return rethrowChecked(() -> nonblockingPOST(path, body).get());
+    }
+
+    @Override
+    public CompletableFuture<String> nonblockingGET(String path) {
+        Request req = client.prepareGet(mkUrl(path)).build();
+        return execute(req);
+    }
+
+    @Override
+    public CompletableFuture<String> nonblockingPOST(String path, String body) {
+        Request req = client.preparePost(mkUrl(path)).setBody(body).build();
+        return execute(req);
+    }
+
+    private CompletableFuture<String> execute(Request request) {
+        return client.executeRequest(request)
+                .toCompletableFuture()
+                .thenApply(resp -> {
+                    if (resp.getStatusCode() != 200) {
+                        // consume response and then throw exception
+                        resp.getResponseBody();
+                        throw new RuntimeException("Unexpected response code : " + resp.getStatusCode());
+                    }
+                    return rethrowChecked(() -> new String(resp.getResponseBodyAsBytes(), StandardCharsets.UTF_8));
+                });
+    }
+
+    private String mkUrl(String path) {
+        if (path.startsWith("/")) {
+            return baseUrl + path;
+        }
+        return baseUrl + "/" + path;
+    }
+
+
+    @Override
+    public void close() throws IOException {
+        client.close();
+    }
+}
